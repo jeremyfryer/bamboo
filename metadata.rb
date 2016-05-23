48d73bc8fadd3479aeb15777dc13c2feab78da7f
jeremyfryer <fryer.jeremy@gmail.com>
2016-05-23 15:23:34 -0500

    Increasing version number

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Ramon Makkelie, Stephan Oudmaijer'
 maintainer_email 'ramonmakkelie@gmail.com, soudmaijer@gmail.com'
 license          'Apache 2.0'
 description      'Installs and configures Bamboo'
-version          '1.6.1'
+version          '1.6.2'
 issues_url       'https://github.com/ramonskie/bamboo/issues'
 source_url       'https://github.com/ramonskie/bamboo.git'
 
